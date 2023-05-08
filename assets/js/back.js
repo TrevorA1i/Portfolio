44f467ce37e20adb51e7fa184aba7c66a79c0579
Trevor Ali <130318126+TrevorA1i@users.noreply.github.com>
2023-05-09 07:17:21 +1200

    Delete assets directory

--- assets/js/back.js
+++
@@ -1,8 +0,0 @@
-
-  const backLinks = document.querySelectorAll(".header.back");
-  for (let i = 0; i < backLinks.length; i++) {
-    backLinks[i].addEventListener("click", function(e) {
-      e.preventDefault();
-      window.history.back();
-    });
-  }
